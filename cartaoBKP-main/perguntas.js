9eb8367b40d9d727ff3c372b6eff6c77a7e916e5
Isabella Souza <146732898+miszwxty@users.noreply.github.com>
2024-10-29 11:01:07 -0300

    Add files via upload

---
+++ cartaoBKP-main/perguntas.js
@@ -0,0 +1,29 @@
+//variaveis
+
+//funções
+
+//chamada
+
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da frança?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Série',
+    'Qual o nome da bruxa de adivinhação da série Agatha desde sempre',
+    'O nome da bruxa é Lilia Calderu'
+)
